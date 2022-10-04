9a770d7bf558dc7f9c0184fcab2aaae00aff037b
Черашкин Александр <cherash86@mail.ru>
2022-10-04 23:08:24 +0500

    fixed

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
 from .models import Post, Group
+
+NUM = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:NUM]
     context = {
         'posts': posts,
     }
@@ -14,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:NUM]
     context = {
         'group': group,
         'posts': posts,
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('', include('posts.urls', namespace='posts'))
 ]
